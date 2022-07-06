c986c50bf8ff34d4ac944022313a1891590c2e60
Laetitia Gonin <l.gonin@codeur.online>
2022-07-06 14:52:47 +0200

    commit3

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Article>
+ *
  * @extends ServiceEntityRepository<Articles>
  *
  * @method Article|null find($id, $lockMode = null, $lockVersion = null)
